9f763e456f351f5ff7fe007c53c5476a6c35ca03
devilhyt <devilhyt@gmail.com>
2023-01-04 15:44:47 +0800

    include dotenv

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
