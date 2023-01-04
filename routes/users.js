496f071e21c78e9f91095700cbfd0827969fe11d
devilhyt <devilhyt@gmail.com>
2023-01-04 15:44:59 +0800

    connect to db

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var { promisePool: mysql } = require('../lib/mysql');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  // res.send('respond with a resource');
+  const [rows, fields] = await mysql.execute('SELECT * FROM `user`');
+  return res.json({
+    'rows': rows,
+  })
 });
+
 
 module.exports = router;
