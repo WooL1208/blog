c79a5e6863835620f961aecc379ac02eec4d0f87
devilhyt <devilhyt@gmail.com>
2023-01-04 16:07:23 +0800

    add some features

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,27 @@ var express = require('express');
 var { promisePool: mysql } = require('../lib/mysql');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
-  // res.send('respond with a resource');
-  const [rows, fields] = await mysql.execute('SELECT * FROM `user`');
-  return res.json({
-    'rows': rows,
-  })
+/* 
+  獲取使用者資訊 
+  get /users
+*/
+router.get('/', async function (req, res, next) {
+  const [rows, fields] = await mysql.execute('SELECT id, is_admin, name, account FROM `user`');
+  return res.json(rows)
+});
+
+
+/* 
+  註冊 
+  post /users
+*/
+router.post('/', async function (req, res, next) {
+  const { name, account, password } = req.body;
+  const hashedPassword = await argon2.hash(password);
+
+  const [rows, fields] = await mysql.execute('INSERT INTO `user` (is_admin, name, account, password) VALUES (0, ?, ?, ?)', [name, account, hashedPassword]);
+
+  res.redirect('/user/');
 });
 
 
