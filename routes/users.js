82784515944f0aed2d0eda46e17d3c628f49c760
zuki1120 <kevin92414kevin@gmail.com>
2023-01-04 20:55:02 +0800

    users試試看

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,35 @@ router.get("/login", function (req, res, next) {
   res.render("users/login", { title: "登入", user, error });
 });
 
+router.post('/login', async function (req, res, next) {
+  const { username, password } = req.body;
+
+  const response = fetch("http://localhost:3000/api/auth", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      account,
+      password,
+    }),
+  }).then(async (resp) => {
+    if (resp.status === 200) {
+      res_json = await resp.json();
+
+      if(res_json.status){
+        res.cookie("tonken", res_json.token);
+        return res.redirect("/");
+      }else{
+        return res.redirect("/users/register?error=登入失敗");
+      }
+    } else {
+      return res.redirect("/users/register?error=API請求失敗");
+    }
+  });
+});
+
+/* 註冊*/ 
 router.get("/register", function (req, res, next) {
   res.render("users/register", { title: "註冊" });
 });
@@ -40,5 +69,7 @@ router.post("/register", async function (req, res, next) {
     }
   });
 });
+
+
 
 module.exports = router;
