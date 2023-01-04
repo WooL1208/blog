e6c68e3474ee27219bab9ac58bda8c497fb86867
devilhyt <devilhyt@gmail.com>
2023-01-04 16:58:45 +0800

    偵測account是否重複

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,6 +12,26 @@ router.get('/', async function (req, res, next) {
 });
 
 
+router.get('/test', async function (req, res, next) {
+  const [rows, fields] = await mysql.execute('SELECT * FROM `user` WHERE account = ?', ['admin']);
+  if (rows.length !== 0) {
+    return res.json(
+      {
+        'status': false,
+        'message': '失敗',
+      }
+    )
+  } else {
+    return res.json(
+      {
+        'status': true,
+        'message': '成功',
+      }
+    )
+  }
+});
+
+
 /* 
   註冊 
   post /users
@@ -20,9 +40,25 @@ router.post('/', async function (req, res, next) {
   const { name, account, password } = req.body;
   const hashedPassword = await argon2.hash(password);
 
-  const [rows, fields] = await mysql.execute('INSERT INTO `user` (is_admin, name, account, password) VALUES (0, ?, ?, ?)', [name, account, hashedPassword]);
+  // 檢查帳號是否重複
+  const [rows_check, fields_check] = await mysql.execute('SELECT * FROM `user` WHERE account = ?', [account]);
+  if (rows.length !== 0) {
+    return res.json(
+      {
+        'status': false,
+        'message': '失敗',
+      }
+    )
+  }
 
-  res.redirect('/user/');
+  // 註冊
+  const [rows, fields] = await mysql.execute('INSERT INTO `user` (is_admin, name, account, password) VALUES (0, ?, ?, ?)', [name, account, hashedPassword]);
+  return res.json(
+    {
+      'status': true,
+      'message': '成功',
+    }
+  )
 });
 
 
