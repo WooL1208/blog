76c5655353209d277ca25ae29c1f4555ff8f2cf8
zuki1120 <kevin92414kevin@gmail.com>
2023-01-06 15:44:20 +0800

    加回title

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,12 @@ require('dotenv').config();
 
 /* 登入頁面 */
 router.get("/login", function (req, res, next) {
-  res.render("users/login");
+  res.render("users/login", { title: 'Login'});
 });
 
 /* 註冊頁面*/
 router.get("/register", function (req, res, next) {
-  res.render("users/register");
+  res.render("users/register", { title: 'Register'});
 });
 
 
