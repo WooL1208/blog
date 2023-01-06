a9b71fc594824aa9a3e0c8567508c3f77ad3abc6
zuki1120 <kevin92414kevin@gmail.com>
2023-01-06 14:13:15 +0800

    小修

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const { token } = req.signedCookies;
-  console.log(req.signedCookies);
   res.render('index', { title: 'Express', token});
 });
 
