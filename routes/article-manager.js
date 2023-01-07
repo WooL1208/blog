841a1fe2116de03634eb7884f25f1a8ab2a3b23c
devilhyt <devilhyt@gmail.com>
2023-01-07 23:54:43 +0800

    拿token

--- routes/article-manager.js
+++ routes/article-manager.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // 後台主頁
 router.get("/", function (req, res, next) {
   const { token } = req.signedCookies;
-  res.render("backstage/index", { title: 'Backstage', token});
+  res.render("article-manger/index", { title: '文章管理', token});
 });
 
 module.exports = router;
