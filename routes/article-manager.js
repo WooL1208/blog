5fa659d0d9bdea4fce2dcf74de73b08eb5efa41d
devilhyt <devilhyt@gmail.com>
2023-01-08 16:51:13 +0800

    文章編輯器

--- routes/article-manager.js
+++ routes/article-manager.js
@@ -2,14 +2,24 @@ var express = require("express");
 var router = express.Router();
 require('dotenv').config();
 
-// 文章管理頁面
+/*
+  文章管理頁面
+  get /article-manager
+*/
 router.get("/", function (req, res, next) {
   const { token } = req.signedCookies;
   res.render("article-manager/index", { title: '文章管理', token });
 });
 
-// 文章編輯器
-router.get("/editor?:mode&:id", function (req, res, next) {
+/*
+  文章編輯器
+  get /article-manager/editor
+  query:
+    mode: add => 新增文章
+    mode: edit => 編輯文章
+    id: => 文章id
+*/
+router.get("/editor", function (req, res, next) {
   const { mode, id } = req.query;
   console.log(mode);
   console.log(id);
