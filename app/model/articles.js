8aa49169d1743b0fee1df3a682f68b51d4781737
devilhyt <devilhyt@gmail.com>
2023-01-08 16:54:34 +0800

    新增content

--- app/model/articles.js
+++ app/model/articles.js
@@ -1,7 +1,7 @@
 ﻿var { promisePool: mysql } = require('../../lib/mysql');
 
 async function getArticlesDb() {
-    const [rows, fields] = await mysql.execute('SELECT article.id, title, category, user_id, user.name, user.account, create_time FROM `article` INNER JOIN `user` ON article.user_id = user.id');
+    const [rows, fields] = await mysql.execute('SELECT article.id, title, category, user_id, user.name, user.account, content, create_time FROM `article` INNER JOIN `user` ON article.user_id = user.id');
     return rows;
 }
 
